137ebbda50c973935b73af5e7166e3f612c7c981
Francisco Roque <francisco@roque.com.pt>
2012-02-01 23:06:55 +0100

    Aesthetic changes, using bootstrap framework

--- src/phenomine/web/resource.py
+++ src/phenomine/web/resource.py
@@ -2,9 +2,10 @@ from fanstatic import Library, Resource
 
 library = Library('phenomine.web', 'static')
 
-style = Resource(library, 'phenomine.css')
-javascript = Resource(library, 'phenomine.js')
-tablesorter = Resource(library, 'tablesorter.css')
+style = Resource(library, 'css/phenomine.css')
+javascript = Resource(library, 'js/phenomine.js')
+tablesorter = Resource(library, 'css/tablesorter.css')
 framework = Resource(library, 'bootstrap/css/bootstrap.css', minified='bootstrap/css/bootstrap.min.css')
 framework_js = Resource(library, 'bootstrap/js/bootstrap.js', minified='bootstrap/js/bootstrap.min.js')
 favicon = Resource(library, 'favicon.ico')
+elastic_textarea = Resource(library, 'js/jquery.elastic.js')
