c8e04a71ea3322af097197c26632cbb424eb7db0
Francisco Roque <francisco@roque.com.pt>
2012-02-02 12:16:56 +0100

    Fixed error handling in form and cleaned up

--- src/phenomine/web/resource.py
+++ src/phenomine/web/resource.py
@@ -4,7 +4,6 @@ library = Library('phenomine.web', 'static')
 
 style = Resource(library, 'css/phenomine.css')
 javascript = Resource(library, 'js/phenomine.js')
-tablesorter = Resource(library, 'css/tablesorter.css')
 framework = Resource(library, 'bootstrap/css/bootstrap.css', minified='bootstrap/css/bootstrap.min.css')
 framework_js = Resource(library, 'bootstrap/js/bootstrap.js', minified='bootstrap/js/bootstrap.min.js')
 favicon = Resource(library, 'favicon.ico')
