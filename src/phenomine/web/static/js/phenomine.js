b4ce502a797ddaa169fd39bf55c2a7cbc1d5d39b
Francisco Roque <francisco@roque.com.pt>
2012-02-02 09:52:34 +0100

    Bootstrap theme finished

--- src/phenomine/web/static/js/phenomine.js
+++ src/phenomine/web/static/js/phenomine.js
@@ -22,12 +22,12 @@ $(document).ready(function()
 		$("#form\\.actions\\.searchHome").addClass('btn').addClass('btn-success');
 		$("#form\\.actions\\.clearHome").addClass('btn');
 		$("#form\\.phenotypes").elastic();
-        // $("#results").tablesorter({headers: { 5: { sorter: false}}}); 
+        $("#results").tablesorter({headers: { 5: { sorter: false}}}); 
         $("tr.gene").each(function(i, tr) {
             var match = $("td.context", tr).html();
             if(match !== null) {
                 var phenotype = $("td.phenotype", tr).html();
-                $("td.context", tr).html(match.replace(new RegExp('(' + phenotype + ')', 'gi'),'<span class="match">$1</span>'))
+                $("td.context", tr).html(match.replace(new RegExp('(' + phenotype + ')', 'gi'),'<strong>$1</strong>'));
             };
         }) ;
     } 
